b7500f0726aa5773ed57d5d6185c860c573207ac
Alex Malikov <alex.malikov94@gmail.com>
2021-03-11 10:00:25 +0500

    Creating and registering job user

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,17 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     use HasFactory;
+
+    /**
+     * Fill your data in database
+     */
+    protected $fillable = [
+      'job_title',
+      'job_location',
+      'job_link',
+      'company_name',
+      'company_logo',
+      'highlighted',
+      'pinned'
+    ];
 
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
